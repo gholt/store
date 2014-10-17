ee2e6e8c8666e72112cdd19395470cb90bec7cb2
Gregory Holt <z-github@brim.net>
2014-10-16 21:13:38 -0500

    salted ktbloomfilter

--- ktbloomfilter.go
+++ ktbloomfilter.go
@@ -12,21 +12,23 @@ type KTBloomFilter struct {
 	m       uint32
 	kDiv4   uint32
 	bits    []byte
+	salt    uint32
 	scratch []byte
 }
 
-func NewKTBloomFilter(n uint64, p float64) *KTBloomFilter {
+func NewKTBloomFilter(n uint64, p float64, salt uint16) *KTBloomFilter {
 	m := -((float64(n) * math.Log(p)) / math.Pow(math.Log(2), 2))
 	return &KTBloomFilter{
 		m:       uint32(math.Ceil(m/8)) * 8,
 		kDiv4:   uint32(math.Ceil(m / float64(n) * math.Log(2) / 4)),
 		bits:    make([]byte, uint32(math.Ceil(m/8))),
+		salt:    uint32(salt) << 16,
 		scratch: make([]byte, 28),
 	}
 }
 
 func (ktbf *KTBloomFilter) String() string {
-	return fmt.Sprintf("KTBloomFilter %p m=%d k=%d bytes=%d", ktbf, ktbf.m, ktbf.kDiv4*4, len(ktbf.bits))
+	return fmt.Sprintf("KTBloomFilter %p m=%d k=%d bytes=%d salt:%d", ktbf, ktbf.m, ktbf.kDiv4*4, len(ktbf.bits), ktbf.salt>>16)
 }
 
 func (ktbf *KTBloomFilter) Add(keyA uint64, keyB uint64, timestamp uint64) {
@@ -35,7 +37,7 @@ func (ktbf *KTBloomFilter) Add(keyA uint64, keyB uint64, timestamp uint64) {
 	binary.BigEndian.PutUint64(scratch[12:], keyB)
 	binary.BigEndian.PutUint64(scratch[20:], timestamp)
 	for i := ktbf.kDiv4; i > 0; i-- {
-		binary.BigEndian.PutUint32(scratch, i)
+		binary.BigEndian.PutUint32(scratch, ktbf.salt|i)
 		h1, h2 := murmur3.Sum128(scratch)
 		bit := uint32(h1>>32) % ktbf.m
 		ktbf.bits[bit/8] |= 1 << (bit % 8)
@@ -54,7 +56,7 @@ func (ktbf *KTBloomFilter) MayHave(keyA uint64, keyB uint64, timestamp uint64) b
 	binary.BigEndian.PutUint64(scratch[12:], keyB)
 	binary.BigEndian.PutUint64(scratch[20:], timestamp)
 	for i := ktbf.kDiv4; i > 0; i-- {
-		binary.BigEndian.PutUint32(scratch, i)
+		binary.BigEndian.PutUint32(scratch, ktbf.salt|i)
 		h1, h2 := murmur3.Sum128(scratch)
 		bit := uint32(h1>>32) % ktbf.m
 		if ktbf.bits[bit/8]&(1<<(bit%8)) == 0 {
--- ktbloomfilter/main.go
+++ ktbloomfilter/main.go
@@ -10,9 +10,9 @@ import (
 )
 
 func main() {
-	itemCount := uint64(10000000)
+	itemCount := uint64(1000000)
 	scratch := make([]byte, 24)
-	scrambled := brimutil.NewSeededScrambled(1)
+	scrambled := brimutil.NewScrambled()
 	items := make([][]uint64, itemCount)
 	for i := uint64(0); i < itemCount; i++ {
 		scrambled.Read(scratch)
@@ -22,38 +22,75 @@ func main() {
 			binary.BigEndian.Uint64(scratch[16:]),
 		}
 	}
-	bf := brimstore.NewKTBloomFilter(itemCount, 0.01)
-	fmt.Println(bf)
 
-	begin := time.Now()
-	for i := uint64(0); i < itemCount; i++ {
-		item := items[i]
-		bf.Add(item[0], item[1], item[2]&0xfffffffffffffffe)
-	}
-	dur := time.Now().Sub(begin)
-	fmt.Printf("%s to add %d items, %.02f/s\n", dur, itemCount, float64(itemCount)/(float64(dur)/float64(time.Second)))
-
-	begin = time.Now()
-	falseNegatives := 0
-	for i := uint64(0); i < itemCount; i++ {
-		item := items[i]
-		if !bf.MayHave(item[0], item[1], item[2]&0xfffffffffffffffe) {
-			falseNegatives++
+	var falses [][]uint64
+	left := func() int {
+		count := 0
+		for i := 0; i < len(falses); i++ {
+			if falses[i] != nil {
+				count++
+			}
 		}
+		return count
 	}
-	dur = time.Now().Sub(begin)
-	fmt.Printf("%s to check for %d items that should exist, %.02f/s\n", dur, itemCount, float64(itemCount)/(float64(dur)/float64(time.Second)))
-	fmt.Println(falseNegatives, "false negatives")
-
-	begin = time.Now()
-	falsePositives := 0
-	for i := uint64(0); i < itemCount; i++ {
-		item := items[i]
-		if bf.MayHave(item[0], item[1], item[2]|1) {
-			falsePositives++
+	iterations := uint16(0)
+	for falses == nil || left() > 0 {
+		iterations++
+		if falses != nil {
+			fmt.Println()
+			fmt.Println(left(), "false positives to resolve")
 		}
+		bf := brimstore.NewKTBloomFilter(itemCount, 0.01, iterations)
+		fmt.Println(bf)
+		begin := time.Now()
+		for i := uint64(0); i < itemCount; i++ {
+			item := items[i]
+			bf.Add(item[0], item[1], item[2]&0xfffffffffffffffe)
+		}
+		dur := time.Now().Sub(begin)
+		fmt.Printf("%s to add %d items, %.02f/s\n", dur, itemCount, float64(itemCount)/(float64(dur)/float64(time.Second)))
+
+		begin = time.Now()
+		falseNegatives := 0
+		for i := uint64(0); i < itemCount; i++ {
+			item := items[i]
+			if !bf.MayHave(item[0], item[1], item[2]&0xfffffffffffffffe) {
+				falseNegatives++
+			}
+		}
+		dur = time.Now().Sub(begin)
+		fmt.Printf("%s to check for %d items that should exist, %.02f/s\n", dur, itemCount, float64(itemCount)/(float64(dur)/float64(time.Second)))
+		fmt.Println(falseNegatives, "false negatives")
+
+		begin = time.Now()
+		falsePositives := 0
+		if falses == nil {
+			falses = make([][]uint64, 0)
+			for i := uint64(0); i < itemCount; i++ {
+				item := items[i]
+				if bf.MayHave(item[0], item[1], item[2]|1) {
+					falses = append(falses, item)
+					falsePositives++
+				}
+			}
+		} else {
+			for i := uint64(0); i < itemCount; i++ {
+				item := items[i]
+				if bf.MayHave(item[0], item[1], item[2]|1) {
+					falsePositives++
+				} else {
+					for j := 0; j < len(falses); j++ {
+						if falses[j] != nil && item[0] == falses[j][0] && item[1] == falses[j][1] && item[2] == falses[j][2] {
+							falses[j] = nil
+						}
+					}
+				}
+			}
+		}
+		dur = time.Now().Sub(begin)
+		fmt.Printf("%s to check for %d items that should not exist, %.02f/s\n", dur, itemCount, float64(itemCount)/(float64(dur)/float64(time.Second)))
+		fmt.Printf("%d false positives, %.02f%%\n", falsePositives, float64(falsePositives)/float64(itemCount)*100)
 	}
-	dur = time.Now().Sub(begin)
-	fmt.Printf("%s to check for %d items that should not exist, %.02f/s\n", dur, itemCount, float64(itemCount)/(float64(dur)/float64(time.Second)))
-	fmt.Printf("%d false positives, %.02f%%\n", falsePositives, float64(falsePositives)/float64(itemCount)*100)
+	fmt.Println()
+	fmt.Println(iterations, "iterations to resolve all false positives")
 }
