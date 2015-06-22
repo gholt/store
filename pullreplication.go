8676436962f806e71be24b2ef6adda5ca0bf37f3
Gregory Holt <gregory_holt@icloud.com>
2015-06-22 13:09:24 -0500

    Fixed max callback calculation

--- pullreplication.go
+++ pullreplication.go
@@ -102,7 +102,7 @@ func (vs *DefaultValueStore) inPullReplication() {
 		tombstoneCutoff := (uint64(brimtime.TimeToUnixMicro(time.Now())) << _TSB_UTIL_BITS) - vs.tombstoneDiscardState.age
 		ktbf := prm.ktBloomFilter()
 		l := int64(_GLH_OUT_BULK_SET_MSG_SIZE)
-		vs.vlm.ScanCallback(prm.rangeStart(), prm.rangeStop(), 0, _TSB_LOCAL_REMOVAL, cutoff, uint64(len(k)), func(keyA uint64, keyB uint64, timestampbits uint64, length uint32) {
+		vs.vlm.ScanCallback(prm.rangeStart(), prm.rangeStop(), 0, _TSB_LOCAL_REMOVAL, cutoff, uint64(cap(k)/2), func(keyA uint64, keyB uint64, timestampbits uint64, length uint32) {
 			if l > 0 {
 				if timestampbits&_TSB_DELETION == 0 || timestampbits >= tombstoneCutoff {
 					if !ktbf.mayHave(keyA, keyB, timestampbits) {
