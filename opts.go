c896f28b5b658dec300c3766a228e98e9d386d6d
Greg Holt <gregory_holt@icloud.com>
2015-07-30 08:24:39 -0500

    Revert "Reducing exposed Opt* funcs."
    This reverts commit 87113d25fb4d2c8e0433c948d4ef07ee17dba796.
    
    Changed my mind on this. Having a lot of options most nobody will use
    sucks, but not having them a year from now when we want to easily tweak
    a setting cluster-wide will probably suck worse. I was just leaving the
    environment variable options lurking, but that's not something easily
    set cluster-wide. I'd like to come up with a slightly cleaner way to
    have all these options without having them clutter up the documentation
    too much, but I haven't figured out anything I really like.

--- opts.go
+++ opts.go
@@ -13,113 +13,56 @@ import (
 	"github.com/gholt/valuelocmap"
 )
 
-// config stores all the available configuration items. If an item is
-// documented here it is because it currently has no public Opt* func;
-// otherwise the documentation will be at the corresponding Opt* func.
 type config struct {
-	logCritical *log.Logger
-	logError    *log.Logger
-	logWarning  *log.Logger
-	logInfo     *log.Logger
-	logDebug    *log.Logger
-	// rand sets the rand.Rand to use as a random data source. Defaults to a
-	// new randomizer based on the current time.
-	rand    *rand.Rand
-	path    string
-	pathtoc string
-	vlm     valuelocmap.ValueLocMap
-	workers int
-	// recoveryBatchSize indicates how many keys to set in a batch while
-	// performing recovery (initial start up). Defaults to env
-	// VALUESTORE_RECOVERYBATCHSIZE or 1,048,576.
-	recoveryBatchSize  int
-	backgroundInterval int
-	messageTimeout     int
-	// tombstoneDiscardInterval indicates the minimum number of seconds
-	// betweeen the starts of discard passes (discarding expired tombstones
-	// [deletion markers]).Defaults to env VALUESTORE_TOMBSTONEDISCARDINTERVAL
-	// or VALUESTORE_BACKGROUNDINTERVAL.
-	tombstoneDiscardInterval int
-	// tombstoneDiscardBatchSize indicates how many items to queue up before
-	// pausing a scan, issuing the actual discards, and resuming the scan
-	// again. Defaults to env VALUESTORE_TOMBSTONEDISCARDBATCHSIZE or
-	// 1,048,576.
-	tombstoneDiscardBatchSize int
-	// inPullReplicationMsgs indicates how many incoming pull-replication
-	// messages can be buffered before dropping additional ones. Defaults to
-	// env VALUESTORE_INPULLREPLICATIONMSGS or VALUESTORE_WORKERS*4.
-	inPullReplicationMsgs int
-	// inPullReplicationHandlers indicates how many incoming pull-replication
-	// messages can be processed at the same time. Defaults to env
-	// VALUESTORE_INPULLREPLICATIONHANDLERS or VALUESTORE_WORKERS.
-	inPullReplicationHandlers int
-	// inPullReplicationMsgTimeout indicates the maximum seconds an incoming
-	// pull-replication message can be pending before just discarding it.
-	// Defaults to env VALUESTORE_INPULLREPLICATIONMSGTIMEOUT or
-	// VALUESTORE_MESSAGETIMEOUT.
+	logCritical                 *log.Logger
+	logError                    *log.Logger
+	logWarning                  *log.Logger
+	logInfo                     *log.Logger
+	logDebug                    *log.Logger
+	rand                        *rand.Rand
+	path                        string
+	pathtoc                     string
+	vlm                         valuelocmap.ValueLocMap
+	workers                     int
+	recoveryBatchSize           int
+	tombstoneDiscardInterval    int
+	tombstoneDiscardBatchSize   int
+	inPullReplicationMsgs       int
+	inPullReplicationHandlers   int
 	inPullReplicationMsgTimeout int
-	// outPullReplicationWorkers indicates how many goroutines may be used for
-	// an outgoing pull replication pass. Defaults to env
-	// VALUESTORE_OUTPULLREPLICATIONWORKERS or VALUESTORE_WORKERS.
-	outPullReplicationWorkers int
-	// outPullReplicationInterval indicates the minimum number of seconds
-	// between the starts of outgoing pull replication passes. Defaults to env
-	// VALUESTORE_OUTPULLREPLICATIONINTERVAL or VALUESTORE_BACKGROUNDINTERVAL.
-	outPullReplicationInterval int
-	// outPullReplicationMsgs indicates how many outgoing pull-replication
-	// messages can be buffered before blocking on creating more. Defaults to
-	// env VALUESTORE_OUTPULLREPLICATIONMSGS or VALUESTORE_WORKERS*4.
-	outPullReplicationMsgs int
-	// outPullReplicationBloomN indicates the N-factor for the outgoing
-	// pull-replication bloom filters. This indicates how many keys the bloom
-	// filter can reasonably hold and, in combination with the P-factor,
-	// affects memory usage. Defaults to env
-	// VALUESTORE_OUTPULLREPLICATIONBLOOMN or 1000000.
-	outPullReplicationBloomN int
-	// outPullReplicationBloomP indicates the P-factor for the outgoing
-	// pull-replication bloom filters. This indicates the desired percentage
-	// chance of a collision within the bloom filter and, in combination with
-	// the N-factor, affects memory usage. Defaults to env
-	// VALUESTORE_OUTPULLREPLICATIONBLOOMP or 0.001.
-	outPullReplicationBloomP float64
-	// outPushReplicationWorkers indicates how many goroutines may be used for
-	// an outgoing push replication pass. Defaults to env
-	// VALUESTORE_OUTPUSHREPLICATIONWORKERS or VALUESTORE_WORKERS.
-	outPushReplicationWorkers int
-	// outPushReplicationInterval indicates the minimum number of seconds
-	// between the starts of outgoing push replication passes. Defaults to env
-	// VALUESTORE_OUTPUSHREPLICATIONINTERVAL or VALUESTORE_BACKGROUNDINTERVAL.
-	outPushReplicationInterval int
-	// outPushReplicationMsgs indicates how many outgoing push-replication
-	// messages can be buffered before blocking on creating more. Defaults to
-	// env VALUESTORE_OUTPUSHREPLICATIONMSGS or VALUESTORE_WORKERS*4.
-	outPushReplicationMsgs  int
-	maxValueSize            int
-	pageSize                int
-	minValueAlloc           int
-	writePagesPerWorker     int
-	tombstoneAge            int
-	valuesFileSize          int
-	valuesFileReaders       int
-	checksumInterval        int
-	msgRing                 ring.MsgRing
-	replicationIgnoreRecent int
-	inBulkSetMsgs           int
-	inBulkSetHandlers       int
-	inBulkSetMsgTimeout     int
-	outBulkSetMsgs          int
-	outBulkSetMsgSize       int
-	inBulkSetAckMsgs        int
-	inBulkSetAckHandlers    int
-	inBulkSetAckMsgTimeout  int
-	outBulkSetAckMsgs       int
-	outBulkSetAckMsgSize    int
-	// compactionInterval indicates how often compaction will run. Defaults to
-	// env VALUESTORE_COMPACTIONINTERVAL or VALUESTORE_BACKGROUNDINTERVAL.
-	compactionInterval     int
-	compactionThreshold    float64
-	compactionAgeThreshold int
-	compactionWorkers      int
+	outPullReplicationWorkers   int
+	outPullReplicationInterval  int
+	outPullReplicationMsgs      int
+	outPullReplicationBloomN    int
+	outPullReplicationBloomP    float64
+	outPushReplicationWorkers   int
+	outPushReplicationInterval  int
+	outPushReplicationMsgs      int
+	outPushReplicationMsgCap    int
+	maxValueSize                int
+	pageSize                    int
+	minValueAlloc               int
+	writePagesPerWorker         int
+	tombstoneAge                int
+	valuesFileSize              int
+	valuesFileReaders           int
+	checksumInterval            int
+	msgRing                     ring.MsgRing
+	replicationIgnoreRecent     int
+	inBulkSetMsgs               int
+	inBulkSetHandlers           int
+	inBulkSetMsgTimeout         int
+	outBulkSetMsgs              int
+	outBulkSetMsgSize           int
+	inBulkSetAckMsgs            int
+	inBulkSetAckHandlers        int
+	inBulkSetAckMsgTimeout      int
+	outBulkSetAckMsgs           int
+	outBulkSetAckMsgSize        int
+	compactionInterval          int
+	compactionThreshold         float64
+	compactionAgeThreshold      int
+	compactionWorkers           int
 }
 
 func resolveConfig(opts ...func(*config)) *config {
@@ -138,19 +81,7 @@ func resolveConfig(opts ...func(*config)) *config {
 			cfg.recoveryBatchSize = val
 		}
 	}
-	cfg.backgroundInterval = 60
-	if env := os.Getenv("VALUESTORE_BACKGROUNDINTERVAL"); env != "" {
-		if val, err := strconv.Atoi(env); err == nil {
-			cfg.backgroundInterval = val
-		}
-	}
-	cfg.messageTimeout = 300
-	if env := os.Getenv("VALUESTORE_MESSAGETIMEOUT"); env != "" {
-		if val, err := strconv.Atoi(env); err == nil {
-			cfg.messageTimeout = val
-		}
-	}
-	cfg.tombstoneDiscardInterval = cfg.backgroundInterval
+	cfg.tombstoneDiscardInterval = 60
 	if env := os.Getenv("VALUESTORE_TOMBSTONEDISCARDINTERVAL"); env != "" {
 		if val, err := strconv.Atoi(env); err == nil {
 			cfg.tombstoneDiscardInterval = val
@@ -162,19 +93,19 @@ func resolveConfig(opts ...func(*config)) *config {
 			cfg.tombstoneDiscardBatchSize = val
 		}
 	}
-	cfg.inPullReplicationMsgs = cfg.workers * 4
+	cfg.inPullReplicationMsgs = 128
 	if env := os.Getenv("VALUESTORE_INPULLREPLICATIONMSGS"); env != "" {
 		if val, err := strconv.Atoi(env); err == nil {
 			cfg.inPullReplicationMsgs = val
 		}
 	}
-	cfg.inPullReplicationHandlers = cfg.workers
+	cfg.inPullReplicationHandlers = 40
 	if env := os.Getenv("VALUESTORE_INPULLREPLICATIONHANDLERS"); env != "" {
 		if val, err := strconv.Atoi(env); err == nil {
 			cfg.inPullReplicationHandlers = val
 		}
 	}
-	cfg.inPullReplicationMsgTimeout = cfg.messageTimeout
+	cfg.inPullReplicationMsgTimeout = 300
 	if env := os.Getenv("VALUESTORE_INPULLREPLICATIONMSGTIMEOUT"); env != "" {
 		if val, err := strconv.Atoi(env); err == nil {
 			cfg.inPullReplicationMsgTimeout = val
@@ -186,13 +117,13 @@ func resolveConfig(opts ...func(*config)) *config {
 			cfg.outPullReplicationWorkers = val
 		}
 	}
-	cfg.outPullReplicationInterval = cfg.backgroundInterval
+	cfg.outPullReplicationInterval = 60
 	if env := os.Getenv("VALUESTORE_OUTPULLREPLICATIONINTERVAL"); env != "" {
 		if val, err := strconv.Atoi(env); err == nil {
 			cfg.outPullReplicationInterval = val
 		}
 	}
-	cfg.outPullReplicationMsgs = cfg.workers * 4
+	cfg.outPullReplicationMsgs = 128
 	if env := os.Getenv("VALUESTORE_OUTPULLREPLICATIONMSGS"); env != "" {
 		if val, err := strconv.Atoi(env); err == nil {
 			cfg.outPullReplicationMsgs = val
@@ -216,13 +147,13 @@ func resolveConfig(opts ...func(*config)) *config {
 			cfg.outPushReplicationWorkers = val
 		}
 	}
-	cfg.outPushReplicationInterval = cfg.backgroundInterval
+	cfg.outPushReplicationInterval = 60
 	if env := os.Getenv("VALUESTORE_OUTPUSHREPLICATIONINTERVAL"); env != "" {
 		if val, err := strconv.Atoi(env); err == nil {
 			cfg.outPushReplicationInterval = val
 		}
 	}
-	cfg.outPushReplicationMsgs = cfg.workers * 4
+	cfg.outPushReplicationMsgs = 128
 	if env := os.Getenv("VALUESTORE_OUTPUSHREPLICATIONMSGS"); env != "" {
 		if val, err := strconv.Atoi(env); err == nil {
 			cfg.outPushReplicationMsgs = val
@@ -336,7 +267,7 @@ func resolveConfig(opts ...func(*config)) *config {
 			cfg.outBulkSetAckMsgSize = val
 		}
 	}
-	cfg.compactionInterval = cfg.backgroundInterval
+	cfg.compactionInterval = 300
 	if env := os.Getenv("VALUESTORE_COMPACTIONINTERVAL"); env != "" {
 		if val, err := strconv.Atoi(env); err == nil {
 			cfg.compactionInterval = val
@@ -389,9 +320,6 @@ func resolveConfig(opts ...func(*config)) *config {
 	}
 	if cfg.recoveryBatchSize < 1 {
 		cfg.recoveryBatchSize = 1
-	}
-	if cfg.backgroundInterval < 1 {
-		cfg.backgroundInterval = 1
 	}
 	if cfg.tombstoneDiscardInterval < 1 {
 		cfg.tombstoneDiscardInterval = 1
@@ -568,6 +496,14 @@ func OptLogDebug(l *log.Logger) func(*config) {
 	}
 }
 
+// OptRand sets the rand.Rand to use as a random data source. Defaults to a new
+// randomizer based on the current time.
+func OptRand(r *rand.Rand) func(*config) {
+	return func(cfg *config) {
+		cfg.rand = r
+	}
+}
+
 // OptPath sets the path where values files will be written; tocvalues files
 // will also be written here unless overridden with OptPathTOC. Defaults to env
 // VALUESTORE_PATH or the current working directory.
@@ -603,19 +539,154 @@ func OptWorkers(count int) func(*config) {
 	}
 }
 
-// OptBackgroundInterval indicates the minimum number of seconds betweeen the
-// starts of background passes (for example, discarding expired tombstones
-// [deletion markers]). If set to 60 seconds and the passes take 10 seconds to
-// run, they will wait 50 seconds (with a small amount of randomization)
-// between the stop of one run and the start of the next. This is really just
-// meant to keep nearly empty structures from using a lot of resources doing
-// nearly nothing. Normally, you'd want your background passes to be running
-// constantly so that they are as fast as possible and the load constant. The
-// default of 60 seconds is almost always fine. Defaults to env
-// VALUESTORE_BACKGROUNDINTERVAL or 60.
-func OptBackgroundInterval(seconds int) func(*config) {
+// OptRecoveryBatchSize indicates how many keys to set in a batch while
+// performing recovery (initial start up). Defaults to env
+// VALUESTORE_RECOVERYBATCHSIZE or 1,048,576.
+func OptRecoveryBatchSize(count int) func(*config) {
 	return func(cfg *config) {
-		cfg.backgroundInterval = seconds
+		cfg.recoveryBatchSize = count
+	}
+}
+
+// OptTombstoneDiscardInterval indicates the minimum number of seconds betweeen
+// the starts of discard passes (discarding expired tombstones [deletion
+// markers]). If set to 60 seconds and the passes take 10 seconds to run, they
+// will wait 50 seconds (with a small amount of randomization) between the stop
+// of one run and the start of the next. This is really just meant to keep
+// nearly empty structures from using a lot of resources doing nearly nothing.
+// Normally, you'd want your discard passes to be running constantly so that
+// they are as fast as possible and the load constant. The default of 60
+// seconds is almost always fine. Defaults to env
+// VALUESTORE_TOMBSTONEDISCARDINTERVAL or 60.
+func OptTombstoneDiscardInterval(seconds int) func(*config) {
+	return func(cfg *config) {
+		cfg.tombstoneDiscardInterval = seconds
+	}
+}
+
+// OptTombstoneDiscardBatchSize indicates how many items to queue up before
+// pausing a scan, issuing the actual discards, and resuming the scan again.
+// Defaults to env VALUESTORE_TOMBSTONEDISCARDBATCHSIZE or 1,048,576.
+func OptTombstoneDiscardBatchSize(count int) func(*config) {
+	return func(cfg *config) {
+		cfg.tombstoneDiscardBatchSize = count
+	}
+}
+
+// OptInPullReplicationMsgs indicates how many incoming pull-replication
+// messages can be buffered before dropping additional ones. Defaults to env
+// VALUESTORE_INPULLREPLICATIONMSGS or 128.
+func OptInPullReplicationMsgs(count int) func(*config) {
+	return func(cfg *config) {
+		cfg.inPullReplicationMsgs = count
+	}
+}
+
+// OptInPullReplicationHandlers indicates how many incoming pull-replication
+// messages can be processed at the same time. Defaults to env
+// VALUESTORE_INPULLREPLICATIONHANDLERS or 40.
+func OptInPullReplicationHandlers(count int) func(*config) {
+	return func(cfg *config) {
+		cfg.inPullReplicationHandlers = count
+	}
+}
+
+// OptInPullReplicationMsgTimeout indicates the maximum seconds an incoming
+// pull-replication message can be pending before just discarding it. Defaults
+// to env VALUESTORE_INPULLREPLICATIONMSGTIMEOUT or 300.
+func OptInPullReplicationMsgTimeout(seconds int) func(*config) {
+	return func(cfg *config) {
+		cfg.inPullReplicationMsgTimeout = seconds
+	}
+}
+
+// OptOutPullReplicationWorkers indicates how many goroutines may be used for
+// an outgoing pull replication pass. Defaults to env
+// VALUESTORE_OUTPULLREPLICATIONWORKERS or VALUESTORE_WORKERS.
+func OptOutPullReplicationWorkers(workers int) func(*config) {
+	return func(cfg *config) {
+		cfg.outPullReplicationWorkers = workers
+	}
+}
+
+// OptOutPullReplicationInterval indicates the minimum number of seconds
+// between the starts of outgoing pull replication passes. If set to 60 seconds
+// and the passes take 10 seconds to run, they will wait 50 seconds (with a
+// small amount of randomization) between the stop of one run and the start of
+// the next. This is really just meant to keep nearly empty structures from
+// using a lot of resources doing nearly nothing. Normally, you'd want your
+// outgoing pull replication passes to be running constantly so that
+// replication is as fast as possible and the load constant. The default of 60
+// seconds is almost always fine. Defaults to env
+// VALUESTORE_OUTPULLREPLICATIONINTERVAL or 60.
+func OptOutPullReplicationInterval(seconds int) func(*config) {
+	return func(cfg *config) {
+		cfg.outPullReplicationInterval = seconds
+	}
+}
+
+// OptOutPullReplicationMsgs indicates how many outgoing pull-replication
+// messages can be buffered before blocking on creating more. Defaults to env
+// VALUESTORE_OUTPULLREPLICATIONMSGS or 128.
+func OptOutPullReplicationMsgs(count int) func(*config) {
+	return func(cfg *config) {
+		cfg.outPullReplicationMsgs = count
+	}
+}
+
+// OptOutPullReplicationBloomN indicates the N-factor for the outgoing
+// pull-replication bloom filters. This indicates how many keys the bloom
+// filter can reasonably hold and, in combination with the P-factor, affects
+// memory usage. Defaults to env VALUESTORE_OUTPULLREPLICATIONBLOOMN or
+// 1000000.
+func OptOutPullReplicationBloomN(count int) func(*config) {
+	return func(cfg *config) {
+		cfg.outPullReplicationBloomN = count
+	}
+}
+
+// OptOutPullReplicationBloomP indicates the P-factor for the outgoing
+// pull-replication bloom filters. This indicates the desired percentage chance
+// of a collision within the bloom filter and, in combination with the
+// N-factor, affects memory usage. Defaults to env
+// VALUESTORE_OUTPULLREPLICATIONBLOOMP or 0.001.
+func OptOutPullReplicationBloomP(percentage float64) func(*config) {
+	return func(cfg *config) {
+		cfg.outPullReplicationBloomP = percentage
+	}
+}
+
+// OptOutPushReplicationWorkers indicates how many goroutines may be used for
+// an outgoing push replication pass. Defaults to env
+// VALUESTORE_OUTPUSHREPLICATIONWORKERS or VALUESTORE_WORKERS.
+func OptOutPushReplicationWorkers(workers int) func(*config) {
+	return func(cfg *config) {
+		cfg.outPushReplicationWorkers = workers
+	}
+}
+
+// OptOutPushReplicationInterval indicates the minimum number of seconds
+// between the starts of outgoing push replication passes. If set to 60 seconds
+// and the passes take 10 seconds to run, they will wait 50 seconds (with a
+// small amount of randomization) between the stop of one run and the start of
+// the next. This is really just meant to keep nearly empty structures from
+// using a lot of resources doing nearly nothing. Normally, you'd want your
+// outgoing push replication passes to be running constantly so that
+// replication is as fast as possible and the load constant. The default of 60
+// seconds is almost always fine. Defaults to env
+// VALUESTORE_OUTPUSHREPLICATIONINTERVAL or 60.
+func OptOutPushReplicationInterval(seconds int) func(*config) {
+	return func(cfg *config) {
+		cfg.outPushReplicationInterval = seconds
+	}
+}
+
+// OptOutPushReplicationMsgs indicates how many outgoing push-replication
+// messages can be buffered before blocking on creating more. Defaults to env
+// VALUESTORE_OUTPUSHREPLICATIONMSGS or 128.
+func OptOutPushReplicationMsgs(count int) func(*config) {
+	return func(cfg *config) {
+		cfg.outPushReplicationMsgs = count
 	}
 }
 
@@ -784,6 +855,14 @@ func OptOutBulkSetAckMsgs(count int) func(*config) {
 func OptOutBulkSetAckMsgSize(bytes int) func(*config) {
 	return func(cfg *config) {
 		cfg.outBulkSetAckMsgSize = bytes
+	}
+}
+
+// OptCompactionInterval indicates how often compaction will run. Defaults to
+// env VALUESTORE_COMPACTIONINTERVAL or 300.
+func OptCompactionInterval(seconds int) func(*config) {
+	return func(cfg *config) {
+		cfg.compactionInterval = seconds
 	}
 }
 
